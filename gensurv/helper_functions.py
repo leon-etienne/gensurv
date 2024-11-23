964c46e8045dd4810f21cc7c83b86940a55a0b90
leetkue <leon.etienne.kuehr@gmail.com>
2024-11-23 17:58:20 +0100

    Init

---
+++ gensurv/helper_functions.py
@@ -0,0 +1,3 @@
+
+def thisisatest():
+    print("working")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gensurv',
+    version='0.1',
+    description='A simple Python module to share functions',
+    author='Leetkue',
+    author_email='your.email@example.com',
+    url='https://github.com/leon-etienne/gensurv',  # GitHub URL
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+)
